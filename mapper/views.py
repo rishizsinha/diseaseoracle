f60a87fdae43bda8c26bbb92ddf33edf45de210e
rishizsinha <rishi.z.sinha@gmail.com>
2015-10-11 03:12:20 -0700

    wired html to django

--- mapper/views.py
+++ mapper/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # template = loader.get_template("../html/DiseaseOracle.html")
+    # return HttpResponse(template.render())
+    return render(request, "pingexample.html")
 
